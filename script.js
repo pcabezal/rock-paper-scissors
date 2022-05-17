206b8ccbe30ca374c1f3375b87c16537547238d5
Paul Cabezal <paulcabezal@gmail.com>
2022-05-17 10:57:46 -0400

    version 1.0 add choice images

--- script.js
+++ script.js
@@ -17,18 +17,28 @@
             }
     }
 
+    function displayMoves() {
+        const playerClasses = document.querySelector('#artLeft').classList;
+        playerClasses.remove(playerClasses.item(0));
+        playerClasses.add(playerSelection);
+
+        const computerClasses = document.querySelector('#artRight').classList;
+        computerClasses.remove(computerClasses.item(0));
+        computerClasses.add(computerSelection);
+    }
+
     function playRound() {      // gets values for Player and Computer choices and determines winner, gives message and adjusts wins/losses
         computerPlay();  
-
+        displayMoves();
         if (playerSelection == computerSelection) {
             document.getElementById("results").innerHTML = `You and computer both picked ` + playerSelection + ".<br>Tie game!";
         } else if (playerSelection == 'rock' && computerSelection == 'scissors' || 
             playerSelection == 'paper' && computerSelection == 'rock' || playerSelection == 'scissors' && computerSelection == 'paper') {
-            document.getElementById("results").innerHTML ='Your ' + playerSelection + ` beats computer's ` + computerSelection + '. <br>You Win! Nice!';
+            document.getElementById("results").innerHTML ='Your ' + playerSelection + ` beats computer's ` + computerSelection + '. <br>You Win!';
             winCount++;
             document.getElementById("humanScore").innerHTML = winCount;
        } else {
-            document.getElementById("results").innerHTML = 'Your ' + playerSelection + ` is defeated by computer's ` + computerSelection + '.<br>You Lose! Pathetic.';
+            document.getElementById("results").innerHTML = 'Your ' + playerSelection + ` is defeated by computer's ` + computerSelection + '.<br>You Lose!';
             lossCount++;
             document.getElementById("compScore").innerHTML = lossCount;
         } 
@@ -48,14 +58,22 @@
         rockButton.removeEventListener("click", rockClick);
         scissorsButton.removeEventListener("click", scissorsClick);
         paperButton.removeEventListener("click", paperClick);
+
         document.getElementById("newGame").innerHTML = '<p id=newGameButton>New Game?</p>';
         const gameNew = document.getElementById('newGameButton');
         gameNew.addEventListener("click", newGameClick);
+
+        const playerClasses = document.querySelector('#artLeft').classList;
+        playerClasses.remove(playerClasses.item(0));
+        
+        const computerClasses = document.querySelector('#artRight').classList;
+        computerClasses.remove(computerClasses.item(0));
     }
 
     function newGameClick() {
+        lossCount = 0;
         winCount = 0;
-        lossCount = 0;        
+        newGameButton.classList.add('clicked');        
         document.getElementById("compScore").innerHTML = lossCount;
         document.getElementById("humanScore").innerHTML = winCount;
         document.getElementById("results").innerHTML = 'Prepare to battle again! First to five wins!'
@@ -63,7 +81,12 @@
         scissorsButton.addEventListener("click", scissorsClick);
         paperButton.addEventListener("click", paperClick);
         removeImg.removeChild(removeImg.childNodes[0]);
-        document.getElementById("newGame").innerHTML = '';
+
+        function removeNewGame() {
+            document.getElementById("newGame").innerHTML = '';
+        }
+        setTimeout(removeNewGame, 200);
+           
     }
 
     // UI
@@ -75,7 +98,6 @@
     const rockButt = document.getElementById("rockButton");
     rockButt.addEventListener("click", rockClick);
     function rockClick() {
-        document.getElementById("results").innerHTML = "rock clicked";
         playerSelection='rock';
         playRound();
         rockButton.classList.add('clicked');
@@ -84,7 +106,6 @@
     const paperButt = document.getElementById("paperButton");
     paperButt.addEventListener("click", paperClick);
     function paperClick() {
-        document.getElementById("results").innerHTML = "paper clicked";
         playerSelection='paper';
         playRound();
         paperButton.classList.add('clicked');
@@ -93,7 +114,6 @@
     const scissorsButt = document.getElementById("scissorsButton");
     scissorsButt.addEventListener("click", scissorsClick);
     function scissorsClick() {
-        document.getElementById("results").innerHTML = "scissors clicked";
         playerSelection='scissors';
         playRound();
         scissorsButton.classList.add('clicked');
